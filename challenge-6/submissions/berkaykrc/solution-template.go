b8945f38a38bb579304243c0bc6622a3ba6453e3
berkaykrc <berkay.kirci@gmail.com>
2025-08-31 03:58:17 +0300

    Add solutions for Challenges by berkaykrc (#330)

---
+++ challenge-6/submissions/berkaykrc/solution-template.go
@@ -0,0 +1,31 @@
+// Package challenge6 contains the solution for Challenge 6.
+package challenge6
+
+import (
+	"regexp"
+	"strings"
+)
+
+// CountWordFrequency takes a string containing multiple words and returns
+// a map where each key is a word and the value is the number of times that
+// word appears in the string. The comparison is case-insensitive.
+//
+// Words are defined as sequences of letters and digits.
+// All words are converted to lowercase before counting.
+// All punctuation, spaces, and other non-alphanumeric characters are ignored.
+//
+// For example:
+// Input: "The quick brown fox jumps over the lazy dog."
+// Output: map[string]int{"the": 2, "quick": 1, "brown": 1, "fox": 1, "jumps": 1, "over": 1, "lazy": 1, "dog": 1}
+func CountWordFrequency(text string) map[string]int {
+	frequency := make(map[string]int)
+	re := regexp.MustCompile(`[\p{L}\p{N}]+(?:['’][\p{L}\p{N}]+)*`)
+	words := re.FindAllString(strings.ToLower(text), -1)
+	for _, w := range words {
+		for _, apostrophe := range []string{"'", "’"} {
+			w = strings.ReplaceAll(w, apostrophe, "")
+		}
+		frequency[w]++
+	}
+	return frequency
+}
